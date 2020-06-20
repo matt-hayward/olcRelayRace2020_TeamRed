5ba0d7f88c6ecc124782050220083238c0f4fa2a
Matt Hayward <matt@matthewhayward.co.uk>
2020-06-20 18:46:22 +0100

    Saladin's Measly Effort!!
    - Too much time spent looking for assets :(

--- src/source.cpp
+++ src/source.cpp
@@ -1,6 +1,12 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
+struct gameObject {
+	olc::vi2d size;
+	olc::vf2d velocity;
+	olc::vf2d position;
+};
+
 class olc_RelayRace : public olc::PixelGameEngine
 {
 public:
@@ -9,18 +15,40 @@ public:
         sAppName = "RelayRace";
     }
 
+	olc::Renderable level;
+	gameObject player, camera;
+	olc::vi2d tileSize, visibleTiles, worldSize; // 212 x 14
+
 public:
     bool OnUserCreate() override
     {
-        return true;
+		level.Load("../../src/20592_rev.png");
+
+
+		player.position = { (float)worldSize.x - 4, 0.0f };
+		player.size = { 16, 32 };
+		player.velocity = { 36.0f, 0.0f };
+
+		camera.position = { 0.0f, 0.0f };
+		tileSize = { 16, 16 };
+		worldSize = { 212, 14 };
+		visibleTiles = { ScreenWidth() / tileSize.x, ScreenHeight() / tileSize.y };
+
+		return true;
     }
 
     bool OnUserUpdate(float fElapsedTime) override
     {
-        Clear(olc::BLACK);
-        for(int y = 0; y < ScreenHeight(); y++)
-            for(int x = 0; x < ScreenWidth(); x++)
-                Draw(x, y, olc::Pixel(rand() % 255, rand() % 255, rand() % 255));
+		DrawDecal({ 0.0f, 0.0f }, level.Decal());
+
+		// move character
+		if (GetKey(olc::Key::LEFT).bHeld) {
+			player.position -= player.velocity * fElapsedTime;
+		} else if (GetKey(olc::Key::RIGHT).bHeld) {
+			player.position += player.velocity * fElapsedTime;
+		}
+
+		FillRectDecal({ player.position.x, (float)ScreenHeight() - 56}, player.size);
         
         return !GetKey(olc::Key::ESCAPE).bPressed;
     }
@@ -32,7 +60,7 @@ private:
 int main()
 {
     olc_RelayRace app;
-    if(app.Construct(320,180,4, 4, false, false))
+    if(app.Construct(640,224,2, 2, false, false))
     {
         app.Start();
     }
